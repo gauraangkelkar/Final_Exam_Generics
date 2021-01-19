ab025fb5073e3458ee7288066c6b240a6112d111
Gauraang P. Kelkar <gauraangkelkar@gmail.com>
2021-01-18 19:52:55 -0500

    First Commit

---
+++ Final_Exam_Program_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Final_Exam_Program_1
+{
+    class SampleList<T>{
+        private List<T> sampleList;
+
+        public void Add(T data)
+        {
+            sampleList.Add(data);
+        }
+
+        public T find(int index)
+        {
+            return sampleList[index];
+        }
+
+        public List<T> sort()
+        {
+            sampleList.OrderByDescending<T,T>(key => {
+                return sampleList;
+            })
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
